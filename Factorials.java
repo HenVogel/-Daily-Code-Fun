afc47ce20a68da771cd7814c35a6f5a8f0486b87
Henry Vogel <145382058+HenVogel@users.noreply.github.com>
2025-01-31 08:22:37 -0500

    Update Factorials.java

--- Factorials.java
+++ Factorials.java
@@ -5,20 +5,26 @@
 // Compute the factorial using a separate function (not main).
 // Print to the screen EXACTLY as follows, substituting x for the user number and the answer for factorial: "The factorial of {x} is {factorial}."
 
+
 import java.util.Scanner;
 
 public class Factorials {
     public static void main(String[] args) {
-        Scanner numEntered = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        
+        int factorial = computeFactorial(number);
+        System.out.println("The factorial of " + number + " is " + factorial + ".");
+    }
 
-        int n, c, fact=1;
-        n = numEntered.nextInt();
-
-        for ( c = 1 ; c <= n ; c++ )
-            fact = fact * c;
-
-        System.out.println("Factorial of " + n + " is = " + fact);
+    public static int computeFactorial(int n) {
+        int result = 1;
+        for (int i = n; i > 1; i--) {
+            result *= i;
+        }
+        return result;
     }
 }
 
